8484b243fa3831f76ebb07e95a61fdfdc9cc57f6
dthuylinhh1 <54965669+dthuylinhh1@users.noreply.github.com>
2020-06-22 10:47:38 +0700

    first commit

---
+++ models/superhero.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// You need to create a new schema and assign it the following
+// constant
+const SuperheroSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  alias: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Superhero', SuperheroSchema);
